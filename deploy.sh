b5401913d7909f41bc4dded7cd536e6712e30903
苑振江 <zhenjiang.yuan@vzhilin.com>
2021-04-15 14:00:52 +0800

    :tada: edit

--- deploy.sh
+++ deploy.sh
@@ -9,13 +9,12 @@ npm run build
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
-#创建.nojekyll 防止Github Pages build错误
-touch .nojekyll
 
 git init
 git add -A
 git commit -m 'deploy'
 
-git push -f "https://${access_token}@github.com/yuguaa/books.git" master:gh-pages
+git push -f git@github.com:yuguaa/books.git master:gh-pages
+# git push -f "https://${access_token}@github.com/yuguaa/books.git" master:gh-pages
 
 cd -
