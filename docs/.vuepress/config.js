21b6ec131c3a03e07faef488bf14640b5aaea3e6
苑振江 <zhenjiang.yuan@vzhilin.com>
2021-04-15 11:57:45 +0800

    :tada: init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,65 @@
+const utils = require('./utils')
+
+module.exports = {
+  title: 'books',
+  description: '个人收藏夹',
+  base: '/books/',
+  head: [
+    [
+      'link',
+      {
+        rel: 'icon',
+        href: '/favicon.ico'
+      }
+    ]
+  ],
+  themeConfig: {
+    nav: [
+      {
+        text: '首页',
+        link: '/'
+      },
+      {
+        text: '库',
+        link: '/repository/'
+      },
+      {
+        text: '网站',
+        link: '/website/'
+      },
+      {
+        text: '文章',
+        link: '/article/'
+      },
+      {
+        text: '面试',
+        link: '/interview/'
+      },
+      {
+        text: 'Blog',
+        link: '/blog/'
+      }
+    ],
+    sidebar: utils.inferSiderbars(),
+    lastUpdated: '上次更新',
+    repo: 'yuguaa/books',
+    editLinks: true,
+    docsDir: 'docs',
+    editLinkText: '在 GitHub 上编辑此页',
+    sidebarDepth: 3
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@public': './public'
+      }
+    }
+  },
+  ga: 'UA-109340118-1',
+  markdown: {
+    config: md => {
+      // use more markdown-it plugins!
+      md.use(require('markdown-it-include'))
+    }
+  }
+}
---
+++ scripts/README-base.md
@@ -0,0 +1,5 @@
+<p align="center">
+  <img width="320" src="https://portrait.gitee.com/uploads/avatars/user/2478/7435551_yuguaa_1586565186.png">
+</p>
+
+在线地址：https://github.com/yuguaa/books
